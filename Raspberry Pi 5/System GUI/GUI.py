b3762c048f8ec0209c053dd11967e25822e16766
Moohan1229 <kal7480@naver.com>
2024-09-12 10:30:06 +0900

    GUI program update

--- Raspberry Pi 5/System GUI/GUI.py
+++ Raspberry Pi 5/System GUI/GUI.py
@@ -1,200 +1,133 @@
-import os
 import tkinter.ttk as ttk
-import tkinter.messagebox as msgbox
-from tkinter import *  # __all__
-from tkinter import filedialog
+from tkinter import *
 
 root = Tk()
-root.title("Monitoring System")
+root.title("Auto Moving Monitoring System")
+root.geometry("720x600")
 
-# 옵션 프레임
-frame_option = LabelFrame(root, text="설정 1")
-frame_option.pack(padx=5, pady=5, ipady=5)
+main_frame = None
+info_label = None
+auto_frame = None
+Manual_frame = None
 
-# 1. 가로 넓이 옵션
-# 가로 넓이 레이블
-lbl_width = Label(frame_option, text="시작 시간 (시)", width=15)
-lbl_width.pack(side="left", padx=20, pady=5)
 
-# 가로 넓이 콤보
-opt_width = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-             "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",]
-cmb_width = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_width, width=10)
-cmb_width.current(0)
-cmb_width.pack(side="left", padx=5, pady=5)
+def clear_frames():
+    global main_frame, info_label, auto_frame, Manual_frame
 
-# 2. 간격 옵션
-# 간격 옵션 레이블
-lbl_space = Label(frame_option, text="시작 시간 (분)", width=15)
-lbl_space.pack(side="left", padx=20, pady=5)
+    if main_frame is not None:
+        main_frame.destroy()
+        main_frame = None
+    if info_label is not None:
+        info_label.destroy()
+        info_label = None
+    if auto_frame is not None:
+        auto_frame.destroy()
+        auto_frame = None
+    if Manual_frame is not None:
+        Manual_frame.destroy()
+        Manual_frame = None
 
-# 간격 옵션 콤보
-opt_space = ["0", "10", "20", "30", "40", "50",]
-cmb_space = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_space, width=10)
-cmb_space.current(0)
-cmb_space.pack(side="left", padx=5, pady=5)  # 옵션 프레임
 
-# ------------------------------------------------------------------------------------
-frame_option = LabelFrame(root, text="설정 2")
-frame_option.pack(padx=5, pady=5, ipady=5)
+def Main():
+    clear_frames()
 
-# 1. 가로 넓이 옵션
-# 가로 넓이 레이블
-lbl_width = Label(frame_option, text="시작 시간 (시)", width=15)
-lbl_width.pack(side="left", padx=20, pady=5)
+    global main_frame
+    main_frame = Frame(root)
+    main_frame.pack(padx=5, pady=5, ipady=5)
 
-# 가로 넓이 콤보
-opt_width = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-             "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",]
-cmb_width = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_width, width=10)
-cmb_width.current(0)
-cmb_width.pack(side="left", padx=5, pady=5)
+    label = Label(main_frame, text="Main Screen", padx=10, pady=10)
+    label.pack(pady=20)
 
-# 2. 간격 옵션
-# 간격 옵션 레이블
-lbl_space = Label(frame_option, text="시작 시간 (분)", width=15)
-lbl_space.pack(side="left", padx=20, pady=5)
 
-# 간격 옵션 콤보
-opt_space = ["0", "10", "20", "30", "40", "50",]
-cmb_space = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_space, width=10)
-cmb_space.current(0)
-cmb_space.pack(side="left", padx=5, pady=5)
+def Auto():
+    clear_frames()
 
-# ------------------------------------------------------------------------------------
-frame_option = LabelFrame(root, text="설정 3")
-frame_option.pack(padx=5, pady=5, ipady=5)
+    global auto_frame
+    auto_frame = Frame(root)
+    auto_frame.pack(padx=5, pady=5, ipady=5)
 
-# 1. 가로 넓이 옵션
-# 가로 넓이 레이블
-lbl_width = Label(frame_option, text="시작 시간 (시)", width=15)
-lbl_width.pack(side="left", padx=20, pady=5)
+    for i in range(1, 7):
+        create_setting_frame(auto_frame, i)
 
-# 가로 넓이 콤보
-opt_width = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-             "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",]
-cmb_width = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_width, width=10)
-cmb_width.current(0)
-cmb_width.pack(side="left", padx=5, pady=5)
 
-# 2. 간격 옵션
-# 간격 옵션 레이블
-lbl_space = Label(frame_option, text="시작 시간 (분)", width=15)
-lbl_space.pack(side="left", padx=20, pady=5)
+def create_setting_frame(parent, setting_number):
+    frame = LabelFrame(parent, text=f"Setting {setting_number}")
+    frame.pack(padx=5, pady=5, ipady=5)
 
-# 간격 옵션 콤보
-opt_space = ["0", "10", "20", "30", "40", "50",]
-cmb_space = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_space, width=10)
-cmb_space.current(0)
-cmb_space.pack(side="left", padx=5, pady=5)
+    # Set Time (Hour)
+    lbl_hour = Label(frame, text="Set Time(Hour)", width=15)
+    lbl_hour.pack(side="left", padx=20, pady=5)
 
-# ------------------------------------------------------------------------------------
-frame_option = LabelFrame(root, text="설정 4")
-frame_option.pack(padx=5, pady=5, ipady=5)
+    opt_hour = [str(i) for i in range(24)]
+    cmb_hour = ttk.Combobox(frame, state="readonly", values=opt_hour, width=10)
+    cmb_hour.current(0)
+    cmb_hour.pack(side="left", padx=5, pady=5)
 
-# 1. 가로 넓이 옵션
-# 가로 넓이 레이블
-lbl_width = Label(frame_option, text="시작 시간 (시)", width=15)
-lbl_width.pack(side="left", padx=20, pady=5)
+    # Set Time (Min)
+    lbl_min = Label(frame, text="Set Time(Min)", width=15)
+    lbl_min.pack(side="left", padx=20, pady=5)
 
-# 가로 넓이 콤보
-opt_width = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-             "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",]
-cmb_width = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_width, width=10)
-cmb_width.current(0)
-cmb_width.pack(side="left", padx=5, pady=5)
+    opt_min = ["0", "10", "20", "30", "40", "50"]
+    cmb_min = ttk.Combobox(frame, state="readonly", values=opt_min, width=10)
+    cmb_min.current(0)
+    cmb_min.pack(side="left", padx=5, pady=5)
 
-# 2. 간격 옵션
-# 간격 옵션 레이블
-lbl_space = Label(frame_option, text="시작 시간 (분)", width=15)
-lbl_space.pack(side="left", padx=20, pady=5)
 
-# 간격 옵션 콤보
-opt_space = ["0", "10", "20", "30", "40", "50",]
-cmb_space = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_space, width=10)
-cmb_space.current(0)
-cmb_space.pack(side="left", padx=5, pady=5)
+def Manual():
+    clear_frames()
 
-# ------------------------------------------------------------------------------------
-frame_option = LabelFrame(root, text="설정 5")
-frame_option.pack(padx=5, pady=5, ipady=5)
+    global Manual_frame
+    Manual_frame = Frame(root)
+    Manual_frame.pack(padx=5, pady=5, ipady=5)
 
-# 1. 가로 넓이 옵션
-# 가로 넓이 레이블
-lbl_width = Label(frame_option, text="시작 시간 (시)", width=15)
-lbl_width.pack(side="left", padx=20, pady=5)
+    label = Label(Manual_frame, text="Manual_frame", padx=10, pady=10)
+    label.pack(pady=20)
 
-# 가로 넓이 콤보
-opt_width = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-             "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",]
-cmb_width = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_width, width=10)
-cmb_width.current(0)
-cmb_width.pack(side="left", padx=5, pady=5)
 
-# 2. 간격 옵션
-# 간격 옵션 레이블
-lbl_space = Label(frame_option, text="시작 시간 (분)", width=15)
-lbl_space.pack(side="left", padx=20, pady=5)
+def Info():
+    clear_frames()
 
-# 간격 옵션 콤보
-opt_space = ["0", "10", "20", "30", "40", "50",]
-cmb_space = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_space, width=10)
-cmb_space.current(0)
-cmb_space.pack(side="left", padx=5, pady=5)
+    global info_label
+    info_label = Frame(root)
+    info_label.pack(padx=5, pady=5)
 
-# ------------------------------------------------------------------------------------
-frame_option = LabelFrame(root, text="설정 6")
-frame_option.pack(padx=5, pady=5, ipady=5)
+    Label(info_label, text="System Spec", padx=10, pady=10).pack(pady=5)
+    Label(info_label, text="Axis Y Length : 8m  Z Length : 1m",
+          padx=10, pady=10).pack(pady=5)
+    Label(info_label, text="Camera IMX296 , GS camera x4",
+          padx=10, pady=10).pack(pady=5)
+    Label(info_label, text="Main: AC220v 60hz , System: DC24V",
+          padx=10, pady=10).pack(pady=5)
+    Label(info_label, text="Made by DongyangTechwin",
+          padx=10, pady=10).pack(pady=150)
 
-# 1. 가로 넓이 옵션
-# 가로 넓이 레이블
-lbl_width = Label(frame_option, text="시작 시간 (시)", width=15)
-lbl_width.pack(side="left", padx=20, pady=5)
 
-# 가로 넓이 콤보
-opt_width = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-             "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",]
-cmb_width = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_width, width=10)
-cmb_width.current(0)
-cmb_width.pack(side="left", padx=5, pady=5)
+menu = Menu(root)
 
-# 2. 간격 옵션
-# 간격 옵션 레이블
-lbl_space = Label(frame_option, text="시작 시간 (분)", width=15)
-lbl_space.pack(side="left", padx=20, pady=5)
 
-# 간격 옵션 콤보
-opt_space = ["0", "10", "20", "30", "40", "50",]
-cmb_space = ttk.Combobox(frame_option, state="readonly",
-                         values=opt_space, width=10)
-cmb_space.current(0)
-cmb_space.pack(side="left", padx=5, pady=5)
-# ------------------------------------------------------------------------------------
+def create_menu_buttons():
+    frame = Frame(root)
+    frame.pack(side="top", padx=10)
 
-path_frame = LabelFrame(root, text="Z 작동시간 추가")
-path_frame.pack(fill="x", padx=5, pady=5, ipady=5)
+    btn_main = Button(frame, text="Main", command=Main, width=17, height=1)
+    btn_main.pack(side="left", padx=1)
 
-txt_dest_path = Entry(path_frame)
-txt_dest_path.pack(side="left", fill="x", expand=True,
-                   padx=5, pady=5, ipady=5)  # 높이 변경
+    btn_auto = Button(frame, text="Auto", command=Auto, width=17, height=1)
+    btn_auto.pack(side="left", padx=1)
 
-btn_dest_path = Button(path_frame, text="Z축 테스트",
-                       width=20, )
-btn_dest_path.pack(side="right", padx=5, pady=5)
+    btn_manual = Button(frame, text="Manual",
+                        command=Manual, width=17, height=1)
+    btn_manual.pack(side="left", padx=1)
 
-cmb_space.current(0)
-cmb_space.pack(side="left", padx=5, pady=5)
-root.resizable(False, False)
-root.mainloop()
+    btn_info = Button(frame, text="Info", command=Info, width=17, height=1)
+    btn_info.pack(side="left", padx=1)
+
+    btn_exit = Button(frame, text="Exit",
+                      command=root.quit, width=17, height=1)
+    btn_exit.pack(side="left", padx=1)
+
+
+create_menu_buttons()
+
+root.config(menu=menu)
 root.mainloop()
